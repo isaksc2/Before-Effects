b587456437338bf211b8d0719c8ef0450dd84e15
isaksc2 <33279671+isaksc2@users.noreply.github.com>
2021-11-21 22:45:42 +0100

    button navbar

--- React/my-app/src/App.js
+++ React/my-app/src/App.js
@@ -1,10 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles'
+import NavBar from './components/NavBar';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#2e1667",
+    },
+    secondary: {
+      main: "#c7d8ed",
+    },
+  },
+  typography: {
+    fontFamily: [
+      'Roboto'
+    ],
+    h4: {
+      fontWeight: 600,
+      fontSize: 28,
+      lineHeight: '2rem',
+    },
+    h5: {
+      fontWeight: 100,
+      lineHeight: '2rem',
+    },
+  },
+});
 
 function App() {
   return (
     <div className="App">
+      <ThemeProvider theme={theme}>
+        <NavBar />
+      </ThemeProvider>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
