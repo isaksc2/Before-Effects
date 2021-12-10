12eeeca5f78bad39ebca9be4f960b789d4375735
isaksc2 <33279671+isaksc2@users.noreply.github.com>
2021-12-10 21:38:20 +0100

    half mask

--- React/my-app/src/components/PlayerContainer.js
+++ React/my-app/src/components/PlayerContainer.js
@@ -1,14 +1,29 @@
 import React from 'react'
 import { Text } from 'react';
-import { Button, TextField } from '@material-ui/core';
-import { Component } from 'react';
+import { Button, TextField, Divider } from '@material-ui/core';
+import { Component, View } from 'react';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
 
 import YouTube from 'react-youtube';
 //const [youtubeID] = useState('fAoRpLbJSVU')
 
+const styles = theme => ({
+    parentDiv: {
+        display: "grid",
+        gridTemplateColumns: "1fr",
+        padding: "0px", // 50
+        backgroundColor: "rgba(0,0,0,0.5)",
+        gridRowStart: 1,
+        gridColumnStart: 1
+    },
+    childDiv: {
+        gridColumn: 1,
+        gridRow: 1
+    }
+});
 
 
-export default class PlayerContainer extends Component {
+class PlayerContainer extends Component {
     // constructor
     constructor(props) {
         super(props);
@@ -37,17 +52,31 @@ export default class PlayerContainer extends Component {
 
     // render
     render() {
+        //const classes = styles();
+        const { classes } = this.props;
         return (
             <div>
                 <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-                <YouTube videoId="fAoRpLbJSVU" onReady={this._onReady} />
-                <YouTube videoId="fAoRpLbJSVU" onReady={this._onReady} />
+                <div className={classes.parentDiv}>
+                    <div className={classes.childDiv}>
+                        <YouTube videoId="SgbF2WRkwgM" onReady={this._onReady} />
+                    </div>
+                    <div className={classes.childDiv}>
+                        <div className={classes.childDiv} style={{ clipPath: "polygon(50% 0%, 50% 100%, 100% 100%, 100% 0%)" }} >
+                            <YouTube videoId="fAoRpLbJSVU" onReady={this._onReady} />
+                        </div>
+                    </div>
+                </div>
+
                 <Button variant="contained" onClick={this.clickPause}>Contained</Button>
             </div>
         )
     }
 }
 
-//export default PlayerContainer;
+
+
+
+export default withStyles(styles)(PlayerContainer);
 
 
